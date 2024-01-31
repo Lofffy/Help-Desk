7f6139f56763480a7cf33008db61335fb16c6d0d
Lofffy <georgemounir003@gmail.com>
2024-01-31 19:00:55 +0200

    changing some files

--- Help-Desk-Software-Backend/backend/server.js
+++ Help-Desk-Software-Backend/backend/server.js
@@ -5,8 +5,6 @@ const express = require('express')
 const app = express()
 const cors = require('cors');
 
-const cors = require('cors');
-
 const PORT = process.env.PORT || 5000 
 
 const connectDB = require('./config/db.js');
